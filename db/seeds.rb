3a73f19aaafcde9092b010cb7ac5c2836902c12d
Bideo Wego <bideowego@gmail.com>
2017-05-11 18:50:55 -0400

    Fix bug in states seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,69 +16,69 @@ MULTIPLIER = 1
 START_TIME = Time.find_zone("Pacific Time (US & Canada)").local(2011, 1, 1)
 END_TIME = Time.find_zone("Pacific Time (US & Canada)").local(2014, 1, 1)
 
-PAYMENT_METHODS = %w(
-  VISA
-  MasterCard
-  AmEx
-  Discover
-  Check
-  Cash
-  StockOptions
-  Doubloons
-)
+PAYMENT_METHODS = [
+  "VISA",
+  "MasterCard",
+  "AmEx",
+  "Discover",
+  "Check",
+  "Cash",
+  "StockOptions",
+  "Doubloons"
+]
 
-STATES = %w(
-  Alabama
-  Alaska
-  Arizona
-  Arkansas
-  California
-  Colorado
-  Connecticut
-  Delaware
-  Florida
-  Georgia
-  Hawaii
-  Idaho
-  Illinois
-  Indiana
-  Iowa
-  Kansas
-  Kentucky
-  Louisiana
-  Maine
-  Maryland
-  Massachusetts
-  Michigan
-  Minnesota
-  Mississippi
-  Missouri
-  Montana
-  Nebraska
-  Nevada
-  New Hampshire
-  New Jersey
-  New Mexico
-  New York
-  North Carolina
-  North Dakota
-  Ohio
-  Oklahoma
-  Oregon
-  Pennsylvania
-  Rhode Island
-  South Carolina
-  South Dakota
-  Tennessee
-  Texas
-  Utah
-  Vermont
-  Virginia
-  Washington
-  West Virginia
-  Wisconsin
-  Wyoming
-)
+STATES = [
+  "Alabama",
+  "Alaska",
+  "Arizona",
+  "Arkansas",
+  "California",
+  "Colorado",
+  "Connecticut",
+  "Delaware",
+  "Florida",
+  "Georgia",
+  "Hawaii",
+  "Idaho",
+  "Illinois",
+  "Indiana",
+  "Iowa",
+  "Kansas",
+  "Kentucky",
+  "Louisiana",
+  "Maine",
+  "Maryland",
+  "Massachusetts",
+  "Michigan",
+  "Minnesota",
+  "Mississippi",
+  "Missouri",
+  "Montana",
+  "Nebraska",
+  "Nevada",
+  "New Hampshire",
+  "New Jersey",
+  "New Mexico",
+  "New York",
+  "North Carolina",
+  "North Dakota",
+  "Ohio",
+  "Oklahoma",
+  "Oregon",
+  "Pennsylvania",
+  "Rhode Island",
+  "South Carolina",
+  "South Dakota",
+  "Tennessee",
+  "Texas",
+  "Utah",
+  "Vermont",
+  "Virginia",
+  "Washington",
+  "West Virginia",
+  "Wisconsin",
+  "Wyoming"
+]
 
 
 
