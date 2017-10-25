46dbdfdd835833c545e104ccce4cc3cbf2ecdd58
Sean Kelly <shonkelli@gmail.com>
2017-10-25 00:21:36 -0400

    completed most questions

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -16,13 +15,13 @@ ActiveRecord::Schema.define(version: 20150722173801) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "airlines", force: true do |t|
+  create_table "airlines", force: :cascade do |t|
     t.string   "name",       null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "airports", force: true do |t|
+  create_table "airports", force: :cascade do |t|
     t.integer  "city_id",    null: false
     t.integer  "state_id",   null: false
     t.string   "code",       null: false
@@ -31,13 +30,13 @@ ActiveRecord::Schema.define(version: 20150722173801) do
     t.datetime "updated_at"
   end
 
-  create_table "cities", force: true do |t|
+  create_table "cities", force: :cascade do |t|
     t.string   "name",       null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "flights", force: true do |t|
+  create_table "flights", force: :cascade do |t|
     t.integer  "origin_id",      null: false
     t.integer  "destination_id", null: false
     t.datetime "departure_time", null: false
@@ -49,27 +48,27 @@ ActiveRecord::Schema.define(version: 20150722173801) do
     t.integer  "distance",       null: false
   end
 
-  create_table "itineraries", force: true do |t|
+  create_table "itineraries", force: :cascade do |t|
     t.integer  "user_id",        null: false
     t.string   "payment_method", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "states", force: true do |t|
+  create_table "states", force: :cascade do |t|
     t.string   "name",       null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "tickets", force: true do |t|
+  create_table "tickets", force: :cascade do |t|
     t.integer  "itinerary_id"
     t.integer  "flight_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "users", force: true do |t|
+  create_table "users", force: :cascade do |t|
     t.integer  "city_id",    null: false
     t.integer  "state_id",   null: false
     t.string   "username",   null: false
